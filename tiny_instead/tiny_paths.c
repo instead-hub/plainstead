d59f8644dcad19537a70194916cceebb3a15bae2
AlexandrKozlovskiy <k.sasha1994@gmail.com>
2023-03-17 01:30:35 +0300

    Небольшая доработка с путями

--- tiny_instead/tiny_paths.c
+++ tiny_instead/tiny_paths.c
@@ -6,38 +6,24 @@ static int luaB_get_savepath(lua_State *L) {
 	char savepath[MAX_PATH];
 	getSavePath(savepath);
 	//MessageBoxA(0, savepath, "", 0);
-		//unix_path(savepath);
-	lua_pushstring(L, savepath);
+		unix_path(savepath);
+			lua_pushstring(L, savepath);
 	//free(savepath);
 	return 1;
 }
-
 static int luaB_get_exepath(lua_State* L) {
-	char instead_exec[MAX_PATH];
-getGamePath(instead_exec);
-	strcat(instead_exec, "..\\..");
-	//unix_path(instead_exec);
-	lua_pushstring(L, instead_exec);
-	//free(instead_exec);
-	return 1;
-}
+		lua_pushstring(L, instead_cwd());
+		return 1;
+	}
 
 static int luaB_get_gamepath(lua_State* L) {
 	char game_path[MAX_PATH];
-getGamePath(game_path);
-	//unix_path(game_path);
+	getGamePath(game_path);
+	//strcat(game_path,"/");
+	unix_path(game_path);
 	lua_pushstring(L, game_path);
-	//free(game_path);
-	return 1;
-}
-
-static int luaB_get_gamespath(lua_State *L) {
-	char games_path[MAX_PATH];
-	getGamePath(games_path);
-	strcat(games_path,"..");
-	//unix_path(games_path);
-	lua_pushstring(L, games_path);
-	//free(games_path);
+		//MessageBoxA(0, game_path, "", 0);
+//free(game_path);
 	return 1;
 }
 
@@ -45,7 +31,7 @@ const luaL_Reg paths_funcs[] = {
 	{"instead_savepath", luaB_get_savepath},
 	{"instead_exepath", luaB_get_exepath},
 	{"instead_gamepath",luaB_get_gamepath},
-	{"instead_gamespath", luaB_get_gamespath},
+	{"instead_gamespath", luaB_get_gamepath},
 	{ NULL, NULL }
 };
 
